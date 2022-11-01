f10a60f78b96f31b9e9beff29cc94541c006e3f7
duongna21 <lablaugrana01@gmail.com>
2022-11-01 11:34:29 +0700

    style

--- ldm/modules/diffusionmodules/openaimodel.py
+++ ldm/modules/diffusionmodules/openaimodel.py
@@ -716,7 +716,7 @@ class UNetModel(nn.Module):
         :param y: an [N] Tensor of labels, if class-conditional.
         :return: an [N x C x ...] Tensor of outputs.
         """
-        print(f'unet input: {x.shape}')
+        print(f'\nunet input: {x.shape}')
         assert (y is not None) == (
             self.num_classes is not None
         ), "must specify y if and only if the model is class-conditional"
