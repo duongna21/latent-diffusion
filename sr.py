f3e2fdeac85376f4c4eec8e2e0f46c0306510abe
duongna21 <lablaugrana01@gmail.com>
2022-11-02 19:01:37 +0700

    style

--- sr.py
+++ sr.py
@@ -3,7 +3,7 @@ from notebook_helpers import run
 from taming.models import vqgan # checking correct import from taming
 from notebook_helpers import get_model
 model = get_model('superresolution')
-print('model: ', model)
+# print('model: ', model)
 
-custom_steps = 1
-logs = run(model["model"], 'data/example_conditioning/superresolution/sample_0.jpg', 'superresolution', custom_steps)
+custom_steps = 100
+logs = run(model["model"], 'data/example_conditioning/superresolution/custom_celeb64.png', 'superresolution', custom_steps)
